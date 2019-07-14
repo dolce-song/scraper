4ce740cf12f1c4b68ee7e029e15bc6f2f4acbcc0
Daiki Morita <daikimorita@DaikinoMacBook-Air.local>
2019-07-14 20:17:17 +0900

    django-circleciの初回コミット

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+import sys
+
+setup(
+    name="crawler",
+    version="0.1",
+    description="a crawler component for the JRA Project",
+    author="Yuma Saito and Daiki Morita",
+    url="https://github.com/dolce-song/crawler"
+)
